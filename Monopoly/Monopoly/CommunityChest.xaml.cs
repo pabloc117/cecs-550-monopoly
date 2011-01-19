08db9cd34a114c4d43c775b0c9da8701f600decf
sloch88@gmail.com <sloch88@gmail.com@24c0f1ae-acb7-4b98-9477-1a9d59cf8995>
2011-01-19 20:35:17 +0000

    Added Chance, Community Chest, Shuffle, and Move - none are functional yet.

---
+++ Monopoly/Monopoly/CommunityChest.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Monopoly
+{
+    /// <summary>
+    /// Interaction logic for CommunityChest.xaml
+    /// </summary>
+    public partial class CommunityChest : Window
+    {
+        public CommunityChest()
+        {
+            InitializeComponent();
+        }
+    }
+}
