788fc205aec3e458265da5958fffa22459750e20
sloch88@gmail.com <sloch88@gmail.com@24c0f1ae-acb7-4b98-9477-1a9d59cf8995>
2011-01-19 20:35:17 +0000

    Added Chance, Community Chest, Shuffle, and Move - none are functional yet.

---
+++ trunk/Monopoly/Monopoly/Chance.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Monopoly
+{
+    /// <summary>
+    /// Interaction logic for Chance.xaml
+    /// </summary>
+    public partial class Chance : Window
+    {
+        List<int> chanceCards = new List<int>(new int[16]);
+        public Chance()
+        {
+            InitializeComponent();
+            //shuffle(chanceCards);
+        }
+
+        private void Assign_Cards(int card)
+        {
+            switch (card)
+            {
+                case 1:
+                    txtBlock.Text = "Advance to Go.\nCollect $200";
+                    break;
+                case 2:
+                    txtBlock.Text = "Advance to Illinois Ave.";
+                    break;
+                case 3:
+                    txtBlock.Text = "Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown.";
+                    break;
+                case 4:
+                    txtBlock.Text = "Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.";
+                    break;
+                case 5:
+                    txtBlock.Text = "You have won a game competition.\nCollect $100";
+                    break;
+                case 6:
+                    txtBlock.Text = "Advance to St. Charles Place.\nIf you pass Go, collect $200.";
+                    break;
+                case 7:
+                    txtBlock.Text = "Bank pays you dividend of $50";
+                    break;
+                case 8:
+                    txtBlock.Text = "Get out of Jail free";
+                    break;
+                case 9:
+                    txtBlock.Text = "Go back 3 spaces";
+                    break;
+                case 10:
+                    txtBlock.Text = "Go directly to Jail – do not pass Go, do not collect $200";
+                    break;
+                case 11:
+                    txtBlock.Text = "Make general repairs on all your property.\nFor each house pay $25 \nFor each hotel $100";
+                    break;
+                case 12:
+                    txtBlock.Text = "Speeding Fine $15";
+                    break;
+                case 13:
+                    txtBlock.Text = "Take a trip to Reading Railroad.\nIf you pass Go collect $200";
+                    break;
+                case 14:
+                    txtBlock.Text = "Advance to Boardwalk";
+                    break;
+                case 15:
+                    txtBlock.Text = "You have been elected chairman of the board.\nPay each player $50 ";
+                    break;
+                case 16:
+                    txtBlock.Text = "Your building and loan matures.\nCollect $150";
+                    break;
+            }
+
+        }
+    }
+}
---
+++ trunk/Monopoly/Monopoly/Shuffle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly
+{
+    class Shuffle
+    {
+        private int[] shuffle(int[] cards)
+        {
+            int temp = 0;
+            Random r = new Random();
+            for (int i = 0; i < cards.Length; i++)
+            {
+                temp = r.Next(1, 16);
+                for (int j = i; j > 0; j--)
+                {
+                    
+                }
+            }
+            return cards;
+        }
+    }
+}
