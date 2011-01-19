a311ab8bdb12b2cdf4563d7971bad059e22ce576
Stephen.Oost@Gmail.com <Stephen.Oost@Gmail.com@24c0f1ae-acb7-4b98-9477-1a9d59cf8995>
2011-01-19 19:55:47 +0000

    -Added file with Property Listing Schema -Added game board and basic property drawing -Added maximize capability

--- Monopoly/Monopoly/GameBoard.xaml.cs
+++ Monopoly/Monopoly/GameBoard.xaml.cs
@@ -19,9 +19,55 @@ namespace Monopoly
     /// </summary>
     public partial class GameBoard : UserControl
     {
+        private enum Sides
+        {
+            LEFT = 0,
+            RIGHT = 11,
+            TOP = 0,
+            BOTTOM = 11
+        }
         public GameBoard()
         {
             InitializeComponent();
+            for (int i = 1; i < 10; i++)
+            {
+                Property rp = new Property((int)Property.Location.RIGHT);
+                rp.PropertyColor = Brushes.Purple;
+                rp.PropertyName = "[THISISALONG TESTNAME]";
+                rp.PropertyCost = 99;
+                Grid.SetColumn(rp, (int)Sides.RIGHT);
+                Grid.SetRow(rp, i);
+                Property tp = new Property((int)Property.Location.TOP);
+                tp.PropertyColor = Brushes.Purple;
+                tp.PropertyName = "[THISISALONG TESTNAME]";
+                tp.PropertyCost = 99;
+                Grid.SetColumn(tp, i);
+                Grid.SetRow(tp, (int)Sides.TOP); 
+                Property lp = new Property((int)Property.Location.LEFT);
+                lp.PropertyColor = Brushes.Purple;
+                lp.PropertyName = "[THISISALONG TESTNAME]";
+                lp.PropertyCost = 99;
+                Grid.SetColumn(lp, (int)Sides.LEFT);
+                Grid.SetRow(lp, i); 
+                Property bp = new Property((int)Property.Location.BOTTOM);
+                bp.PropertyColor = Brushes.Purple;
+                bp.PropertyName = "[THISISALONG TESTNAME]";
+                bp.PropertyCost = 99;
+                Grid.SetColumn(bp, i);
+                Grid.SetRow(bp, (int)Sides.BOTTOM);
+                myBoard.Children.Add(tp);
+                myBoard.Children.Add(rp);
+                myBoard.Children.Add(lp);
+                myBoard.Children.Add(bp);
+            }
+        }
+
+        private void board_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.HeightChanged)
+            {
+                this.Width = e.NewSize.Height;
+            }
         }
     }
 }
--- Monopoly/Monopoly/MainWindow.xaml.cs
+++ Monopoly/Monopoly/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections;
 
 namespace Monopoly
 {
@@ -19,9 +20,52 @@ namespace Monopoly
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point _dim;
+        private Point _loc;
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                if (this.WindowState != System.Windows.WindowState.Normal)
+                    Restore();
+                else
+                    Maximize();
+                
+            }
+            else if (e.Key == Key.Escape)
+                Restore();
+        }
+        
+        private void Maximize()
+        {
+            if (this.Dispatcher.CheckAccess())
+            {
+                _dim = new Point(this.Width, this.Height);
+                _loc = new Point(this.Left, this.Top);
+                this.WindowState = System.Windows.WindowState.Maximized;
+                this.WindowStyle = System.Windows.WindowStyle.None;
+            }
+            else this.Dispatcher.BeginInvoke(new Action(Maximize));
+        }
+
+        public void Restore()
+        {
+            if (this.Dispatcher.CheckAccess())
+            {
+                this.Left = _loc.X;
+                this.Top = _loc.Y;
+                this.Width = _dim.X;
+                this.Height = _loc.Y;
+                this.WindowState = System.Windows.WindowState.Normal;
+                this.WindowStyle = System.Windows.WindowStyle.ThreeDBorderWindow;
+            }
+            else this.Dispatcher.BeginInvoke(new Action(Restore));
         }
     }
 }
