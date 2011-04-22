f3d6bfffac52f1ed8ad144275e98a503cf7d0753
Stephen.Oost@Gmail.com <Stephen.Oost@Gmail.com@24c0f1ae-acb7-4b98-9477-1a9d59cf8995>
2011-04-22 19:57:55 +0000

    added a much needed class

---
+++ trunk/Monopoly/Monopoly/IPRequest.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Monopoly
+{
+    /// <summary>
+    /// Interaction logic for IPRequest.xaml
+    /// </summary>
+    public partial class IPRequest : Window
+    {
+        public IPRequest()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            //check valid input
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string ip = ip1.Text + "." + ip2.Text + "." + ip3.Text + "." + ip4.Text;
+            OnIPAccept(new ConnectClickedEventArgs(ip));
+            this.Close();
+        }
+
+        public event EventHandler<ConnectClickedEventArgs> IPAccept;
+        private void OnIPAccept(ConnectClickedEventArgs e)
+        {
+            IPAccept(this, e);
+        }
+
+        private void ip_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ((TextBox)sender).SelectAll();
+        }
+    }
+
+    public class ConnectClickedEventArgs : EventArgs
+    {
+        public string IP = "";
+        public ConnectClickedEventArgs(string IP)
+        { 
+            this.IP = IP;
+        }
+    }
+}
